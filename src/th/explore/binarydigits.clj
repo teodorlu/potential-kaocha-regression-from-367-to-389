82e6fe4f00c2bab8b1b4b08137a8c94708fd613d
Teodor Lunaas Heggelund <th@purelogic.no>
2019-06-12 16:23:00 +0200

    Flesh out API outline

--- src/th/explore/binarydigits.clj
+++ src/th/explore/binarydigits.clj
@@ -1,12 +1,5 @@
 (ns th.explore.binarydigits)
 
-(def hello-message "This is a message to greet you hello!")
-
-(defn raise
-  "If you want to fly higher ..."
-  []
-  (str "|||| " hello-message "!!!!!!"))
-
 (defn foo "Before the name comes the function"
   [])
 
@@ -15,3 +8,17 @@
 
 (defn probably-false? [prob]
   (< prob 0.01))
+
+(def target-activations {0 [0 0 0 0]
+                         1 [0 0 0 1]
+                         2 [0 0 1 0]
+                         3 [0 0 1 1]
+                         4 [0 1 0 0]
+                         5 [0 1 0 1]
+                         6 [0 1 1 0]
+                         7 [0 1 1 1]
+                         8 [1 0 0 0]
+                         9 [1 0 0 1]})
+
+;; But I need a way to run the neurons as well. How do I run one with some input?
+;; And for each one, ..., they could just be run with "arguments" as the xs.
--- test/th/explore/binarydigits_test.clj
+++ test/th/explore/binarydigits_test.clj
@@ -3,11 +3,25 @@
             [clojure.test :refer :all]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Goal
+;; GOAL
 ;;
 ;; Describe an interface that ensures that we hit our "digit modeling" right.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; RESOURCES
+;;
+;; Book URL: http://neuralnetworksanddeeplearning.com/chap1.html
 
 (deftest truthy-heuristics
   (testing "Values > 0.99 are considered true"
     (is (digits/probably-true? 0.991))
-    (is (not (digits/probably-true? 0.899)))))
+    (is (not (digits/probably-true? 0.899))))
+  (testing "Values < 0.01 are considered false"
+    (is (digits/probably-false? 0.006))
+    (is (not (digits/probably-false? 0.02)))
+    (is (not (digits/probably-false? 0.012)))))
+
+;; Now, what does our transformation look like? We need those neurons.
+
+(deftest transform-api
+  (is digits/foo))
