0e724b7d9acbc411a294a2e4683bd6da03354217
Teodor Lunaas Heggelund <th@purelogic.no>
2019-06-13 10:36:37 +0200

    Document progression

--- src/th/explore/binarydigits.clj
+++ src/th/explore/binarydigits.clj
@@ -3,29 +3,42 @@
 (defn foo "Before the name comes the function"
   [])
 
-(defn probably-true? [prob]
+(defn probably-true?
+  "We represent true as values 0.99 < prob < 1"
+  [prob]
   (> prob 0.99))
 
-(defn probably-false? [prob]
+(defn probably-false?
+  "We represent false as values 0 < prob < 0.01"
+  [prob]
   (< prob 0.01))
 
-(defn make-sigmoid [weights bias]
+(defn make-sigmoid
+  "Create an object with weights and bias for use later"
+  [weights bias]
   {:weights weights
    :bias bias})
 
-(defn sigmoid-z [z]
+(defn sigmoid-z
+  "The sigmoid function transforms (-inf, inf) to (0, 1) smoothly"
+  [z]
   (/ 1
      (+ 1 (Math/exp (- z)))))
 
-(defn dotproduct [x y]
+(defn dotproduct
+  "The dot product (inner product) of two vectors"
+  [x y]
   (reduce + (map * x y)))
 
-(defn run-sigmoid [sigmoid x]
+(defn run-sigmoid
+  "Execute a sigmoid on inputs x"
+  [sigmoid x]
   (let [{:keys [weights bias]} sigmoid]
     (sigmoid-z (+ (dotproduct weights x)
                   bias))))
 
 (def binary-target-activations
+  "A zero (base 10) is represented as [0 0 0 0] (base 0, less than 16)"
   {0 [0 0 0 0]
    1 [0 0 0 1]
    2 [0 0 1 0]
